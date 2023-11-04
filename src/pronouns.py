6b332d3b42b86db58b954f70d3ac329bc86b04b9
Ivan Gusev <ivangusevproj@gmail.com>
2023-11-04 19:58:26 +0300

    pronoun validation algo is implemented

---
+++ src/pronouns.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def build_pronoun_dictionary(line):
+    words = line.split(" ")
+    pronouns =  set(pd.read_json('./../data/pronouns.json')[0].tolist())
+    result = {}
+    for key in pronouns:
+        result[key] = 0
+    for word in words:
+        if (word in pronouns):
+            result[word] += 1
+    return result
+
+def check_pronoun_correspondence(line1, line2):
+    dict1 = build_pronoun_dictionary(line1)
+    dict2 = build_pronoun_dictionary(line2)
+    return dict1 == dict2
