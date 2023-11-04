1d34443509dcc0c137520401da1b11bad2ed954b
ghitsh <none@whatever.com>
2023-11-04 20:11:46 +0300

    Levenshtein distance algorithm implementation

---
+++ src/lev_dist.py
@@ -0,0 +1,26 @@
+def levenshtein_distance(left, right):
+    m = len(left)
+    n = len(right)
+
+    d = [[0] * (n + 1) for i in range(m + 1)]  
+
+    for i in range(1, m + 1):
+        d[i][0] = i
+
+    for j in range(1, n + 1):
+        d[0][j] = j
+    
+    for j in range(1, n + 1):
+        for i in range(1, m + 1):
+            if left[i - 1] == right[j - 1]:
+                cost = 0
+            else:
+                cost = 1
+            d[i][j] = min(d[i - 1][j] + 1,        # deletion
+                          d[i][j - 1] + 1,        # insertion
+                          d[i - 1][j - 1] + cost) # substitution   
+
+    return 1 - (d[m][n] / max(len(left), len(right)))
+
+def is_rewrite_levenshtein_distance(left: str, right: str, threshold: float)
+    return levenshtein_distance(left, right) > threshold
