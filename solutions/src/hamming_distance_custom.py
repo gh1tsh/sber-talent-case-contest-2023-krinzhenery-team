8a1f59a262e254bab4e051c00c48e2260bb28179
Danis <danis.ru2002@mail.ru>
2023-11-05 20:05:00 +0300

    Добавил кастомное расстояние Хэмминга

---
+++ solutions/src/hamming_distance_custom.py
@@ -0,0 +1,7 @@
+def is_rewrite_hamming_distance_custom(left:str,right:str,threshold=0.9)->bool:
+    if len(left) != len(right):
+        return False
+    c = 0
+    for i in range(len(left)):
+        c += left[i]!=right[i]
+    return c == 1
