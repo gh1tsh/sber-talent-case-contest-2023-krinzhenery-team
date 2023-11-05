313b94e594895e5efdb86c70c4230c7bbb6b7a34
DanisDeveloper <71634912+DanisDeveloper@users.noreply.github.com>
2023-11-05 17:39:08 +0300

    Update hamming_distance_custom.py

--- solutions/src/hamming_distance_custom.py
+++ solutions/src/hamming_distance_custom.py
@@ -4,4 +4,4 @@ def is_rewrite_hamming_distance_custom(left:str,right:str,threshold=0.9)->bool:
     c = 0
     for i in range(len(left)):
         c += left[i]!=right[i]
-    return c == 1
+    return 1 - c / len(left) > threshold
