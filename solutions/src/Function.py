2cc11888558ee54b2d550d63394213bcc954ff61
Danis <danis.ru2002@mail.ru>
2023-11-19 17:37:27 +0300

    Добавил комментации

--- solutions/src/Function.py
+++ solutions/src/Function.py
@@ -1,9 +1,12 @@
+# Обертка для базовой функции (используется в решении №3)
+# Позволяет функции проголосовать
 class Function:
     def __init__(self,function,threshold:float,weight:int):
         self.function = function
         self.threshold = threshold
         self.weight = weight
     
+    # метод голосования
     def vote(self,left,right):
         if self.threshold == 0:
             if self.function(left,right):
